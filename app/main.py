dc62da6eaaa752cf2892a4a89c4481eefec56f9f
Sacha Capellutom <sacha.capelluto@ulb.be>
2025-11-19 18:18:51 +0100

    fix: Disable CORS credentials to allow wildcard origin

--- app/main.py
+++ app/main.py
@@ -17,7 +17,7 @@ app = FastAPI(
 app.add_middleware(
     CORSMiddleware,
     allow_origins=settings.CORS_ORIGINS,
-    allow_credentials=True,
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
